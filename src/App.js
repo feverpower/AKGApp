28e0c16a3c179ef46a289ae6da21b12ac9c3f6e5
feverpower <krishnaraj.rajagopal@gmail.com>
2019-04-18 16:34:28 -0400

    move App function to a separate file

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Architecture Knowledge Graph</h1>
+      <h2>Product Line Views</h2>
+      <select>
+        <option value="Customer Master Data Management">
+          Customer Master Data Management
+        </option>
+        <option value="Digital Experience">Digital Experience</option>
+      </select>
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-function App() {
-  return (
-    <div>
-      <h1>Architecture Knowledge Graph</h1>
-      <h2>Product Line Views</h2>
-      <select>
-        <option value="Customer Master Data Management">
-          Customer Master Data Management
-        </option>
-        <option value="Digital Experience">Digital Experience</option>
-      </select>
-    </div>
-  );
-}
+import App from "./App.js";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
